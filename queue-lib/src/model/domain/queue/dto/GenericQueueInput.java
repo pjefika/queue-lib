270663c24be9ae73b243729a040e884542eb604d
Agibert <matheusagibert@gmail.com>
2018-01-11 18:29:45 -0200

    fix type duplicado

--- queue-lib/src/model/domain/queue/dto/GenericQueueInput.java
+++ queue-lib/src/model/domain/queue/dto/GenericQueueInput.java
@@ -15,7 +15,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  */
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
+//        include = JsonTypeInfo.As.PROPERTY,
         visible = true,
         property = "type")
 @JsonSubTypes({
--- queue-lib/src/model/domain/queue/dto/GenericQueueOutput.java
+++ queue-lib/src/model/domain/queue/dto/GenericQueueOutput.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  */
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
+//        include = JsonTypeInfo.As.PROPERTY,
         property = "type",
         visible = true)
 @JsonSubTypes({
